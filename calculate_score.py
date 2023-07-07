a06809dd4180cd1468e691c43d6b8f1f0eac7676
Munawar Ali <138569548+MunawarAliArbisoft@users.noreply.github.com>
2023-07-07 17:28:32 +0500

    Add typings, .env file, divide programs into modules, and make small optimization/clean code changes
    In this commit I added typings, created .env file to store environment variables for api, divided programs into modules to make codebase organized and clean and made various small optimizations and improvements to enhance performance and readability.

---
+++ calculate_score.py
@@ -0,0 +1,30 @@
+
+def cal_accuracy_score(given_word: str, 
+                       user_text: str, no_of_backspaces: int) -> float:
+    """Calculates the accuracy score for typing tester"""
+
+    wrong_letter = 0
+    try:
+        wrong_letter = sum([1 for i in range(len(given_word)) if given_word[i] != user_text[i]])
+    except IndexError:
+        wrong_letter += 1
+
+    total_mistakes = wrong_letter + no_of_backspaces
+    accuracy =  (len(given_word) - total_mistakes)/len(given_word)*100
+    # assign 0 if accuracy is negative
+    accuracy = 0 if accuracy < 0 else accuracy
+
+    return float(f"{accuracy:.2f}"), wrong_letter
+
+
+def cal_time_score(elapsed_time: float, given_word: str) -> float:
+    """Calculates the time score for typing tester"""
+
+    # Avg. time for one keystroke
+    KEYSTROKE_TIME = 0.28 
+    # Avg. time to hover hands from mouse to keyboard
+    MOUSE_TO_KEYBOARD_TIME = 0.4 
+    # formula to calculate raw score
+    raw_score = elapsed_time / (MOUSE_TO_KEYBOARD_TIME + (KEYSTROKE_TIME * (len(given_word) + 1)))
+
+    return float(100) if raw_score < 1 else float(f"{(100/raw_score):.2f}")
